2343e4d8e9d9901fb66cf2964760367c408ace37
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-24 20:21:49 -0300

    feat: formatando o valor da despesa em reais

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById('amount');
 
+// Captura o evento de input para formatar o valor.
  amount.oninput = () => {
-    const value = amount.value.replace(/\D/g, "");
-    amount.value = value;
+    // Obtém o valor atual do input e remove os caracteres não numéricos 
+    let value = amount.value.replace(/\D/g, "");
+
+    // Transforma o valor em centavos.
+    value = Number(value) / 100;
+
+    // Atualiza o valor do input.
+    amount.value = formatCurrencyBRL(value);
+ }
+
+ function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (Real Brasileiro).
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+
+    // Retorna o valor formatado.
+    return value;
  }
