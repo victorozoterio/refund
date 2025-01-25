b4d0d9ed622d417a02492dd2c4e87c2e4e9b604a
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-24 21:00:01 -0300

    feat: criando objeto de nova despesa e mostrando ícone da categoria

--- scripts.js
+++ scripts.js
@@ -1,25 +1,71 @@
-// Seleciona os elementos do formulário
-const amount = document.getElementById('amount');
+// Seleciona os elementos do formulário.
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+// Seleciona os elementos da lista.
+const expenseList = document.querySelector("ul");
 
 // Captura o evento de input para formatar o valor.
- amount.oninput = () => {
-    // Obtém o valor atual do input e remove os caracteres não numéricos 
-    let value = amount.value.replace(/\D/g, "");
+amount.oninput = () => {
+	// Obtém o valor atual do input e remove os caracteres não numéricos
+	let value = amount.value.replace(/\D/g, "");
 
-    // Transforma o valor em centavos.
-    value = Number(value) / 100;
+	// Transforma o valor em centavos.
+	value = Number(value) / 100;
 
-    // Atualiza o valor do input.
-    amount.value = formatCurrencyBRL(value);
- }
+	// Atualiza o valor do input.
+	amount.value = formatCurrencyBRL(value);
+};
 
- function formatCurrencyBRL(value) {
-    // Formata o valor no padrão BRL (Real Brasileiro).
-    value = value.toLocaleString("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-    });
+function formatCurrencyBRL(value) {
+	// Formata o valor no padrão BRL (Real Brasileiro).
+	value = value.toLocaleString("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+	});
 
-    // Retorna o valor formatado.
-    return value;
- }
+	// Retorna o valor formatado.
+	return value;
+}
+
+form.onsubmit = (event) => {
+	// Previne o comportamento padrão de recarregar a página.
+	event.preventDefault();
+
+	// Cria um objeto com os detalhes da nova empresa.
+	const newExpense = {
+		id: new Date().getTime(),
+		expense: expense.value,
+		category_id: category.value,
+		category_name: category.options[category.selectedIndex].text,
+		amount: amount.value,
+		created_at: new Date(),
+	};
+
+	// Chama a função que irá adicionar o item na lista.
+	expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+	try {
+		// Cria o elemento para adicionar o item (li) na lista (ul).
+		const expenseItem = document.createElement("li");
+		expenseItem.classList.add("expense");
+
+		// Cria o ícone da categoria.
+		const expenseIcon = document.createElement("img");
+		expenseIcon.setAttribute("src", `./img/${newExpense.category_id}.svg`);
+		expense.setAttribute("alt", newExpense.category_name);
+
+		// Adiciona as informações no item.
+		expenseItem.append(expenseIcon);
+
+		// Adiciona o item na lista.
+		expenseList.append(expenseItem);
+	} catch (error) {
+		alert("Não foi possível atualizar a lista de despesas.");
+		console.log(error);
+	}
+}
