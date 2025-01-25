48da84d6b43dab72d60706624d62e02ea88ec315
Victor Ozoterio <victorozoterio@gmail.com>
2025-01-24 22:11:12 -0300

    feat: exibindo a quantidade de itens da lista

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category");
 
 // Seleciona os elementos da lista.
 const expenseList = document.querySelector("ul");
+const expenseQuantity = document.querySelector("aside header p span");
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -48,6 +49,7 @@ form.onsubmit = (event) => {
 	expenseAdd(newExpense);
 };
 
+// Adiciona um novo item na lista.
 function expenseAdd(newExpense) {
 	try {
 		// Cria o elemento para adicionar o item (li) na lista (ul).
@@ -90,8 +92,25 @@ function expenseAdd(newExpense) {
 
 		// Adiciona o item na lista.
 		expenseList.append(expenseItem);
+
+		// Atualiza os totais.
+		updateTotals();
 	} catch (error) {
 		alert("Não foi possível atualizar a lista de despesas.");
+		console.log(error);
+	}
+}
+
+// Atualiza os totais.
+function updateTotals() {
+	try {
+		// Recupera todos os itens (li) da lista (ul).
+		const items = expenseList.children;
+
+		// Atualiza a quantidade de itens da lista.
+		expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`;
+	} catch (error) {
+		alert("Não foi possível atualizar os totais.");
 		console.log(error);
 	}
 }
